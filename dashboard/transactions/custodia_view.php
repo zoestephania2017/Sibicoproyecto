9732f57e5478243e8f4481ea42e4b120cdcb27de
DenzelTorrico <denzel_silva@hotmail.com>
2024-07-20 19:31:59 -0500

    Cargo y descargo view

--- dashboard/transactions/custodia_view.php
+++ dashboard/transactions/custodia_view.php
@@ -1,6 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+<?php
+        include __DIR__ . '/../../app/utils/Helpers.php';
+        require __DIR__ . '/../../app/utils/Protection.php';
+        require __DIR__ . '/../../app/utils/Session.php';
+        
+        $session = new Session();
+        $protection = new Protection($session->get('access') ?? []);
+        
+        if (!isset($protection) || !$protection->has("8")) {
+            header('Location: ../?denied=true&_per=' . urlencode("8"), true, 302);
+            exit;
+        }
+        ?>
+        
+    
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
@@ -49,10 +65,57 @@
             background-color: #0056b3;
         }
     </style>
+       <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/6cf319f10f.js" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="<?= url('src/css/style.css') ?>">
+    <script>
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/BodegaCopeco/service-worker.js');
+        }
+    </script>
 </head>
 <body>
+<body class="bg-gray-100">
+    <div class="form-container">
+        <header>
+            <button data-drawer-target="sidebar" data-drawer-toggle="sidebar"
+                aria-controls="sidebar-multi-level-sidebar" type="button"
+                class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-800 rounded-lg sm:hidden hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-800">
+                <span class="sr-only">Abrir menu de navegación</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd" fill-rule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
+                    </path>
+                </svg>
+            </button>
+            <aside id="sidebar"
+                class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+                aria-label="Sidebar">
+                <div class="h-full px-3 py-4 overflow-y-auto bg-slate-300">
+                    <div class="p-4 bg-white rounded-lg mb-8 shadow-lg">
+                        <?php include __DIR__ . '/../components/box_data.php'; ?>
+                    </div>
+
+                    <ul class="space-y-4 font-medium">
+                        <?php require_once __DIR__ . '/../components/sidebar.php'; ?>
+                    </ul>
+                </div>
+            </aside>
+        </header>
+        <main class="p-4 h-auto sm:ml-32">
+            <div class="p-4 border-dashed rounded-lg">
+                <div class="flex flex-col gap-4 w-full">
+                    <div class="w-full h-auto bg-slate-300 rounded p-4 text-black shadow-lg">
+                    <h1>Custodia por Empleado</h1>
+                    </div>
+                 
+                </div>
+            </div>
+        </main>
 <div class="form-container">
-    <h1>Custodia por Empleado</h1>
     <form action="CustodiaController.php" method="post">
         <div class="form-group">
             <label for="employeeName">Nombre Completo de Empleado:</label>
@@ -66,6 +129,10 @@
         </div>
     </form>
 </div>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" defer></script>
+    <script src="<?= url('src/js/module/swal.min.js') ?>" defer></script>
+    <script src="<?= url('src/js/module/jquery.min.js') ?>" defer></script>
+    <script src="<?= url('src/js/module/axios/axios.min.js') ?>" defer></script>
+    <script src="<?= url('src/js/maintenance/access/1/app-min.js') ?>" type="module" defer></script>
 </body>
 </html>
